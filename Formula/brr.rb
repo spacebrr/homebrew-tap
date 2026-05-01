b83b60e931001edd6448aff7f348203873646ebf
tyson <tyson.chan@proton.me>
2026-05-01 11:22:54 +1000

    release(tap): bump brr to 0.3.30

--- Formula/brr.rb
+++ Formula/brr.rb
@@ -4,27 +4,35 @@ class Brr < Formula
   version "0.3.30"
   license :cannot_represent
 
+  service do
+    run [opt_bin/"brr-daemon"]
+    keep_alive true
+    log_path "#{Dir.home}/.space/logs/daemon.log"
+    error_log_path "#{Dir.home}/.space/logs/daemon.log"
+    environment_variables PATH: std_service_path_env
+  end
+
   on_macos do
     if Hardware::CPU.arm?
       url "https://releases.spacebrr.com/v0.3.30/brr-darwin-arm64"
-      sha256 "a99369bcc56e028d206ff2542cc3c454a7b9658bda8acdf0a8dee48b833383c3"
+      sha256 "c2b694a01ac8acd6296f03e2efafde1ddbf50790b5910efa64afcfb19a25accc"
 
       resource "brr-daemon" do
         url "https://releases.spacebrr.com/v0.3.30/brr-daemon-darwin-arm64"
-        sha256 "c92ec7296f2933c3f23c26e2969d2ca7e65acd6ef25faafd85c69e3c1b0e946c"
+        sha256 "59d1430a68ebb3e620f60a3299ed2610766291c507c4400bdaec9960d9fe3aca"
       end
 
       resource "brr-spawn" do
         url "https://releases.spacebrr.com/v0.3.30/brr-spawn-darwin-arm64"
-        sha256 "6eb7fbd90dbc1634951589a00335536feb14d46859e4ee9af832fa8968dd9320"
+        sha256 "c0548fbbaa171ea53d25ce4443aef6be93517cbffc31b7ef5298033596dd354a"
       end
     else
       url "https://releases.spacebrr.com/v0.3.30/brr-darwin-amd64"
-      sha256 "cde10649caf929a60c532a602932e8957d13724d72b31f9e0364617ac9efccf9"
+      sha256 "c2258831c81e1983f02bbd19ac0b8f6f4479d00e4713625fd56950e0745a10c2"
 
       resource "brr-daemon" do
         url "https://releases.spacebrr.com/v0.3.30/brr-daemon-darwin-amd64"
-        sha256 "b48ccca6eb2fbfde683a2d698f4abb273ebde83e48f20426a8b25fb876cd48c8"
+        sha256 "3b3f033bb018d796c45cf885b5fe4f923c7acdbfc761ee5258df03562ae36503"
       end
 
       resource "brr-spawn" do
@@ -38,10 +46,6 @@ class Brr < Formula
     bin.install Dir.glob("brr-*").first || "brr" => "brr"
     resource("brr-daemon").stage { bin.install Dir.glob("brr-*").first || "brr-daemon" => "brr-daemon" }
     resource("brr-spawn").stage { bin.install Dir.glob("brr-*").first || "brr-spawn" => "brr-spawn" }
-  end
-
-  def post_install
-    system "#{bin}/brr", "reset"
   end
 
   test do
