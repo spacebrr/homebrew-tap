3c8ac42b140e2bf1f078f90f001ad24f1d645ae3
tyson <tyson.chan@proton.me>
2026-05-04 10:04:38 +1000

    fix(brr): correct sha256 checksums for 0.3.46 binaries

--- Formula/brr.rb
+++ Formula/brr.rb
@@ -15,11 +15,11 @@ class Brr < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://releases.spacebrr.com/v0.3.46/brr-darwin-arm64"
-      sha256 "839d6362009d08249050f5c415410f34425228e9a7bf97b5f164899f63fe2fdb"
+      sha256 "86c809493c5cbc71e1d0301b2e0d952fa39b5a6d3d55c6bd8f2c0fdc4ab70755"
 
       resource "brr-daemon" do
         url "https://releases.spacebrr.com/v0.3.46/brr-daemon-darwin-arm64"
-        sha256 "237f81dac9efb1a3255a80f29f6caedf4f4e464c3f84c126272a6298bcc45158"
+        sha256 "b1368f0ada03e9aee5e5f9f01cd60d3863749c3a6fdcc59a61769a3285e2302f"
       end
 
       resource "brr-spawn" do
@@ -28,11 +28,11 @@ class Brr < Formula
       end
     else
       url "https://releases.spacebrr.com/v0.3.46/brr-darwin-amd64"
-      sha256 "c3601ddc20b20308a79ee586bbf3d75d8c85eedbd406541ebd426070722344b7"
+      sha256 "794b13dd7c9f23525a2e3ff0b9c3719949904902fa4cbf83169f8bef0eecfe42"
 
       resource "brr-daemon" do
         url "https://releases.spacebrr.com/v0.3.46/brr-daemon-darwin-amd64"
-        sha256 "335e88bfbd05db474a0f92ccff24de4386ea7e4e1defc9e6ef1c7bbe7d443aee"
+        sha256 "921d8b220a7ed4b63391ba42e4ea95583df9a426d2bfb6273bf52d844d21933e"
       end
 
       resource "brr-spawn" do
